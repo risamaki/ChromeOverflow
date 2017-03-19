b04501f7093d6ed83d04f0b0768903bbc874693c
vincentw <vincentw@live.ca>
2017-03-18 19:59:06 -0700

    ADD SOMETHING TO BODY FROM DEVTOOLS.JS

--- devtools.js
+++ devtools.js
@@ -2,5 +2,13 @@
 chrome.devtools.panels.create("ChromeOverflow",
   "resources/icons/ChromeOverflow128.png",
   "panel.html",
-  function(panel) {}
+    function(extensionPanel) {
+      var runOnce = false;
+      extensionPanel.onShown.addListener(function (panelWindow) {
+        if (runOnce) return;
+        runOnce = true;
+        // Do something, eg appending the text "Hello!" to the devtools panel
+        panelWindow.document.body.appendChild(document.createTextNode('Hello!'));
+      });
+    }
 );
