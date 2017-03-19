1adcd7392b1579489e996d75fbe4d068908b18cd
vincentw <vincentw@live.ca>
2017-03-18 22:05:49 -0700

    Detect window errors and print to console

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,45 @@
+function injectErrorListener() {
+	window.addEventListener('error', function(e) {
+		// e has other properties https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+		var error = {
+			columnNumber: e.error.columnNumber,
+			fileName: e.error.fileName,
+			lineNumber: e.error.lineNumber,
+			message: e.error.message,
+			name: e.error.name,
+			stack: e.error.stack,
+			toString: e.error.toString()
+		};
+
+		document.dispatchEvent(new CustomEvent('WindowError', { detail: error}));
+	});
+}
+
+document.addEventListener('WindowError', function(e) {
+	// STACK is the only useful one.
+	// toString = name + message
+	// stack = toString + stack_stuff
+	console.log('columnNumber: ', e.detail.columnNumber);
+	console.log('fileName: ', e.detail.fileName);
+	console.log('lineNumber: ', e.detail.lineNumber);
+	console.log('message: ', e.detail.message);
+	console.log('name: ', e.detail.name);
+	console.log('stack: ', e.detail.stack);
+	console.log('toString: ', e.detail.toString);
+});
+
+//Inject code
+var script = document.createElement('script');
+script.textContent = '(' + injectErrorListener + '())';
+(document.head || document.documentElement).appendChild(script);
+//script.parentNode.removeChild(script);
+
+
 var url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&tagged=JavaScript&site=stackoverflow&accepted=True&filter=!Su916jaHmcZfZh5aMc"
 function search(query, page, callback) {
 	if (typeof query !== "string") throw new Error("Pass in a string!");
 	let requestUrl = url + "&title=" + encodeURIComponent(query) +
-	"&page=" + page + "pagesize=5";
+		"&page=" + page + "pagesize=5";
 	let ret = {};
 	$.get(requestUrl).done(function(data) {
 		processResults(data, function(val) {
@@ -23,7 +60,7 @@ function search(query, page, callback) {
 	});
 }
 
-// take object retrieve only 
+// take object retrieve only
 function processResults(data, callback) {
 	let ret = {
 		hasMore: false,
