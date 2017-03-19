b1c81bd957b7f5f57e2fa05a89857748bd94a348
Justin Chau <justin.chau623@gmail.com>
2017-03-18 22:56:32 -0700

    Fix merge conflict messages being left behind

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function injectErrorListener() {
 	window.addEventListener('error', function(e) {
 		// e has other properties https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
@@ -36,10 +35,7 @@ script.textContent = '(' + injectErrorListener + '())';
 //script.parentNode.removeChild(script);
 
 
-var url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&tagged=JavaScript&site=stackoverflow&accepted=True&filter=!Su916jaHmcZfZh5aMc"
-=======
 var url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&tagged=JavaScript&site=stackoverflow&accepted=True&filter=!Su916jaHmcZfZh5aMc";
->>>>>>> master
 function search(query, page, callback) {
     if (typeof query !== "string") throw new Error("Pass in a string!");
     var requestUrl = url + "&title=" + encodeURIComponent(query) +
